52e27a99be6c2991d23bc9339ff9ee6e4c247353
Mardjona <Mardjona26@gmail.com>
2024-06-12 22:16:10 +0300

    first commit

---
+++ addproduct/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Input.TextInput;
+using Avalonia.Interactivity;
+
+namespace addproduct;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
+
+  
